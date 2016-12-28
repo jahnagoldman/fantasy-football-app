9cd2ee36391f5a90c167dc153126c73df1da8f04
Jahna Arielle Goldman <jagoldman@cs.uchicago.edu>
2016-12-28 16:00:18 -0600

    git ignore and mysql

--- fantasyfootball/settings.py
+++ fantasyfootball/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'fantasyfootball.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ffootballdb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
